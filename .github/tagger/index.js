bbd3a140dece494f3a236c1958e09332cc0f4c40
Rob Shepherd <rs@datacymru.net>
2022-08-18 22:37:13 +0100

    test tagger regex and tag this v1.0.7

--- .github/tagger/index.js
+++ .github/tagger/index.js
@@ -1,3 +1,6 @@
+//
+// build: ncc build index.js --license licenses.txt
+//
 const core = require('@actions/core');
 const github = require('@actions/github');
 
@@ -6,14 +9,25 @@ async function run() {
     // The YML workflow will need to set myToken with the GitHub Secret Token
     // myToken: ${{ secrets.GITHUB_TOKEN }}
     // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
+    console.log(process.env.GITHUB_TOKEN, process.env.GITHUB_REPOSITORY, process.env.GITHUB_SHA)
 
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    var payload = github.context.payload
+    var msg = payload.head_commit.message
+
+    var patt = /((?:v)?[0-9]{1,}\.[0-9]{1,}\.[0-9]{1,}(?!\-SNAPSHOT)(?:[-a-zA-Z0-9]+)?)/ // anything but snapshots
+    var match = patt.exec(msg)
+
+    if(!match || match.length < 2) {
+        console.log("Not tagging - no match: " + msg)
+        return
+    }
+
+    var tag = match[1]
+
+    console.log("Taggin with: " + tag)
 
     /*
-    const myToken = core.getInput('myToken');
-
-    const octokit = github.getOctokit(myToken)
+    const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
 
     // You can also pass in additional options as a second parameter to getOctokit
     // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
